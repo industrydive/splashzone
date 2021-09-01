3781c00e60799add3ef65de7ba8234a0753cabb0
Bo Morin <morinbe@gmail.com>
2021-09-01 09:46:39 -0400

    Add better formatting to instructions, flesh out WWAR prompts

--- apps/helpers/templatetags/helper_tags.py
+++ apps/helpers/templatetags/helper_tags.py
@@ -16,3 +16,17 @@ def story_image(story):
     if os.path.exists(image_path):
         return 'image/news/{}.jpg'.format(story.pk)
     return 'image/placeholder-img.jpg'
+
+
+@register.filter(name='gherkinize_step')
+def gherkinize_step(step):
+    gherkin_starters = [
+        'Given', 'When', 'Then', 'And'
+    ]
+    split_str = step.split(' ')
+    if split_str[0] in gherkin_starters:
+        split_str[0] = '<strong>{}</strong>'.format(split_str[0])
+        return ' '.join(split_str)
+    if '<table>' in step:
+        return '<code>{}</code>'.format(step)
+    return step
--- apps/wavepool/onboarding_exercise_defs.py
+++ apps/wavepool/onboarding_exercise_defs.py
@@ -1,23 +1,27 @@
 prompts = [
     {
-        'title': 'Add a "What we are reading" section to News Posts section (back-end)',
+        'title': 'Add a "What we are reading" section to News Post page (back-end)',
         'description': '''
             In order to boost our editorial reputation, as a Dive Site Editor,
             I want to be able to add links to relevant sources which would appear at the end of newsposts.
             Editors should be able to add up to 3 "What we are reading" (WWAR) links per News Post. Some
-            WWAR links will likely be used by more than one News Post.
+            WWAR links will likely be used by more than one News Post, but they should be only available to
+            the dive site on which they were created. Duplicates WWAR links on a given dive site should be prevented.
         ''',
         'AC': [
             'A new model and subsequent migration exist in the news app which will store WWAR links',
             'Model properties match the fields described in the scenarios',
             'Admin form matches the interactions described in the scenarios',
             'Limit News Posts to 3 WWAR links',
-            'Any given WWAR link should be able to be associated with any News Post for the dive site it is on'
+            'Any given WWAR link should be able to be associated with any News Post for the dive site it is on',
+            'Users should not be able to save the same link twice on a dive site'
         ],
         'objectives': [
             'Understand how to create new models in Django',
             'Understand how to manage multiple models from one admin page in Django',
-            'Understand many-to-many relationships in the Django ORM'
+            'Understand many-to-many relationships in the Django ORM',
+            'Understand how to create composite unique keys in Django',
+            'Understand how SiteModel effects admin page interactions',
         ],
         'scenarios': [
             {
@@ -29,15 +33,102 @@ prompts = [
                 ]
             },
             {
-                'title': 'An admin user can add/edit/delete a WWAR link using standard admin pages',
+                'title': 'An admin user can access a standard django admin Create form for WWAR links',
                 'steps': [
                     'Given I am a Dive Site Editor and I on the "What We Are Reading" admin list page',
                     'When I click "ADD WHAT WE ARE READING +"',
-                    'Then I see a form with the fields',
-                    'label: "title", type: text input',
-                    'label: "link", type: url input',
-                    'label: "source", type: text input',
-                    'label: "published", type: date input, help text: "the date that the linked article was originally published"',  # noqa
+                    'Then I see a form with the fields:',
+                    '''
+                    <table>
+                        <tr><th>label</th><th>input type</th><th>help text</th></tr>
+                        <tr><td>"title"</td><td>Text</td><td></td></tr>
+                        <tr><td>"source"</td><td>URL</td><td></td></tr>
+                        <tr>
+                            <td>"published"</td>
+                            <td>Date</td>
+                            <td>"the date that the linked article was originally published"</td>
+                        </tr>
+                    </table>
+                    '''
+                ]
+            },
+            {
+                'title': 'An admin user can add a WWAR link using standard admin pages',
+                'steps': [
+                    'Given I am a Dive Site Editor and I on the "What We Are Reading" create page',
+                    'When I fill in the form with:',
+                    '''
+                    <table>
+                        <tr><th>title</th><td>Macarena turns 25</td></tr>
+                        <tr><th>link</th><td>https://www.theonion.com/macarena-turns-25-1847417240</td></tr>
+                        <tr><th>source</th><td>The Onion</td></tr>
+                        <tr><th>published</th><td>08/03/2021</td></tr>
+                    </table>
+                    ''',
+                    'And I press "save"',
+                    'Then I see a success message'
+                ]
+            },
+            {
+                'title': 'An admin user <strong>can not</strong> add the same WWAR link twice on a <strong>single dive site</strong>',  # noqa
+                'steps': [
+                    'Given I am a Dive Site Editor for YYY Dive',
+                    'And I on the "What We Are Reading" create page',
+                    'And the WWAR links exist:',
+                    '''
+                    <table>
+                        <tr><th>site</th><th>link</th></tr>
+                        <td>YYY</td>
+                        <td>https://www.theonion.com/macarena-turns-25-1847417240</td>
+                        </tr>
+                        <tr>
+                        <td>ZZZ</td>
+                        <td>https://www.theonion.com/elizabeth-holmes-arrives-to-trial-with-prototype-for-bl-1847584107</td>
+                        </tr>
+                    </table>
+                    ''',
+                    'When I fill in the form with:',
+                    '''
+                    <table>
+                        <tr><th>title</th><td>Macarena turns 25</td></tr>
+                        <tr><th>link</th><td>https://www.theonion.com/macarena-turns-25-1847417240</td></tr>
+                        <tr><th>source</th><td>The Onion</td></tr>
+                        <tr><th>published</th><td>08/03/2021</td></tr>
+                    </table>
+                    ''',
+                    'And I press save',
+                    'Then I see a unique constraint error message',
+                ]
+            },
+            {
+                'title': 'An admin user <strong>can</strong> add the same WWAR link twice on a <strong>different dive sites</strong>',  # noqa
+                'steps': [
+                    'Given I am a Dive Site Editor for ZZZ Dive',
+                    'And I on the "What We Are Reading" create page',
+                    'And the WWAR links exist:',
+                    '''
+                    <table>
+                        <tr><th>site</th><th>link</th></tr>
+                        <td>YYY</td>
+                        <td>https://www.theonion.com/macarena-turns-25-1847417240</td>
+                        </tr>
+                        <tr>
+                        <td>ZZZ</td>
+                        <td>https://www.theonion.com/elizabeth-holmes-arrives-to-trial-with-prototype-for-bl-1847584107</td>
+                        </tr>
+                    </table>
+                    ''',
+                    'When I fill in the form with:',
+                    '''
+                    <table>
+                        <tr><td>title</td><td>Macarena turns 25</td></tr>
+                        <tr><td>link</td><td>https://www.theonion.com/macarena-turns-25-1847417240</td></tr>
+                        <tr><td>source</td><td>The Onion</td></tr>
+                        <tr><td>published</td><td>08/03/2021</td></tr>
+                    </table>
+                    ''',
+                    'And I press save',
+                    'Then I see a success message',
                 ]
             },
             {
@@ -54,7 +145,7 @@ prompts = [
         ]
     },
     {
-        'title': 'Add a "What we are reading" section to News Posts section (front-end)',
+        'title': 'Add a "What we are reading" section to News Post page (front-end)',
         'description': '''
             In order to get more information about topics covered on a given Dive Site story, as a reader,
             I want a list with links to related coverage from other sources.
@@ -78,7 +169,7 @@ prompts = [
                 'title': 'A reader sees WWAR links in a section for the news post they are currently viewing',
                 'steps': [
                     'Given I am a Dive Site Reader',
-                    'And the WWAR links exist',
+                    'And the WWAR links exist:',
                     '''
                         <table>
                         <tr><th>News Post ID</th><th>WWAR Link</th></tr>
